dd07659ac857e544bd10a2de124c97cfaf0b24cd
crackkie <craz493@gmail.com>
2025-01-31 13:15:19 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -29,7 +29,16 @@ function changeSlide(){
 // changeSlide()
 setInterval(changeIndex,3000,1);
 
-window.onload = greetingPopup();
+// window.onload = greetingPopup();
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 400){
+        document.getElementById('header').classList.add('scroled')
+    }else{
+        document.getElementById('header').classList.remove('scroled')
+    }
+})
+
 
 function greetingPopup(){
     let button = document.getElementById('submit-greeting');
@@ -54,49 +63,3 @@ function greetingPopup(){
 
     })
 };
-
-document.getElementById('submit').addEventListener('click', ()=>{
-    console.log('click')
-    validation()
-})
-
-function validation(){
-    let notify = document.getElementById('alert')
-    let doneNotify = document.getElementById('doneNotify');
-    let name = document.getElementById('fname').value;
-    let dob = document.getElementById('fbirth').value;
-    let genderMale = document.getElementById('genderMale');
-    let genderFemale = document.getElementById('genderFemale');
-    let message = document.getElementById('message').value;
-
-    if (name ===''){
-        notify.innerHTML = 'Name cannot be empty!';
-        return;
-    }
-    if(dob ===''){
-        notify.innerHTML = 'Date Of Birth cannot be empty!';
-        return;
-    }
-   if(genderMale.checked === false && genderFemale.checked === false){
-        notify.innerHTML = 'Gender cannot be empty!';
-        return ;
-   }
-   if(message ===''){
-        notify.innerHTML = 'At leas write one word!';
-        return;
-   }else{
-        notify.innerHTML ='';
-        doneNotify.innerHTML = 'Sent successfully'
-        document.getElementById('time').innerHTML = Date();
-        document.getElementById('name-message').innerHTML = name;
-        document.getElementById('birth-message').innerHTML = dob;
-
-        if(genderMale.checked === true){
-            document.getElementById('gender-message').innerHTML = 'Male';
-        }else{
-            document.getElementById('gender-message').innerHTML = 'Female';
-        }
-
-        document.getElementById('message-container').innerHTML = message;
-   }
-}
